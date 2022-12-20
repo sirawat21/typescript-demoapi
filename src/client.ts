c69e36c5666438bb68cec9e45fc35e494830de5f
sirawat21 <sirawat21@gmail.com>
2022-12-20 15:22:26 +1100

    optimise phantom request REST function

--- src/client.ts
+++ src/client.ts
@@ -1,114 +1,114 @@
 import * as phantom from 'phantom';
 
+/* Main */
+const main = (async () => {
+  const data = {
+    user: 'John Doe',
+    text: 'john.doe@example.com'
+  };
+  await sendPostRequest('/message', data);
+  let result = await sendGetRequest('/message');
+  console.log(result);
+})();
+
+/* Phantom Request Handler */
 const port = 3030;
-const path = 'api'
-const baseUrl = `http://localhost:${port}/${path}`;
+const url = `http://localhost:${port}/api`;
 
 // [GET ALL]
-async function sendGetRequest(): Promise<string> {
+async function sendGetRequest(path: string, params: string = ''): Promise<string> {
+  // Set up request
   const instance = await phantom.create();
   const page = await instance.createPage();
-  const status = await page.open(`${baseUrl}/message`);
-  console.log(status);
-
-  const content = await page.property('content');
-  console.log(content);
-
-  await instance.exit();
+  const status = await page.open(`${url}${path}/${params}`);
+  // Status handler
+  let content: string;
+  if (status === 'success') content = await page.property('content');
+  else content = 'Requesting is fail.';
+  instance.exit();
+  // Return result
   return JSON.stringify(content);
 }
 
 // [GET ID]
-async function sendGetRequestWithId(id: string): Promise<string> {
+async function sendGetRequestWithId(path: string, params: string = ''): Promise<string> {
+  // Set up request
   const instance = await phantom.create();
   const page = await instance.createPage();
-  const status = await page.open(`${baseUrl}/message/${id}`);
-  console.log(status);
-
-  const content = await page.property('content');
-  console.log(content);
-
-  await instance.exit();
+  const status = await page.open(`${url}${path}/${params}`);
+  // Status handler
+  let content: string;
+  if (status === 'success') content = await page.property('content');
+  else content = 'Requesting is fail.';
+  instance.exit();
+  // Return result
   return JSON.stringify(content);
 }
 
 // [POST]
-async function sendPostRequest(): Promise<void> {
-  const instance = await phantom.create();
-  const page = await instance.createPage();
-  await page.on('onResourceRequested', function (requestData) {
-    console.info('Requesting', requestData.url);
-  });
-
-  // sset ttp operation & send data
-  const data = {
-    user: 'John Doe',
-    text: 'john.doe@example.com'
-  };
+async function sendPostRequest(path: string, data: Object, param: string = ''): Promise<string> {
+  // Set operation
   const options: phantom.IOpenWebPageSettings = {
     operation: 'POST',
     data: JSON.stringify(data)
   }
-
-  const method: 'POST' = 'POST';
-  const status = await page.open(`${baseUrl}/message`, options);
-  console.log(status);
-
-  await page.setContent(baseUrl + '/message', JSON.stringify(data)); // set send var
-
-  const content = await page.property('content');
-  console.log(content);
-
-  await instance.exit();
-}
-
-// [PUT]
-async function sendPutRequest(id: string): Promise<void> {
+  // Set up request
   const instance = await phantom.create();
   const page = await instance.createPage();
   await page.on('onResourceRequested', function (requestData) {
     console.info('Requesting', requestData.url);
   });
+  const status = await page.open(`${url}${path}/${param}`, options);
+  // Status handler
+  let content: string;
+  if (status === 'success') content = await page.property('content');
+  else content = 'Requesting is fail.';
+  instance.exit();
+  // Return result
+  return JSON.stringify(content);
+}
 
-  // set ttp operation & send data
-  const data = {
-    user: 'John Doe',
-    text: 'john.doe@example.com'
-  };
+// [PUT]
+async function sendPutRequest(path: string, data: Object, param: string = ''): Promise<string> {
+  // Set operation
   const options: phantom.IOpenWebPageSettings = {
     operation: 'PUT',
     data: JSON.stringify(data)
   }
-
-  const status = await page.open(`${baseUrl}/message/${id}`, options);
-  console.log(status);
-
-  await page.setContent(baseUrl + '/message', JSON.stringify(data)); // set send var
-
-  const content = await page.property('content');
-  console.log(content);
-
-  await instance.exit();
-}
-
-// [DELETE]
-async function sendDeleteRequest(id: string): Promise<void> {
+  // Set up request
   const instance = await phantom.create();
   const page = await instance.createPage();
   await page.on('onResourceRequested', function (requestData) {
     console.info('Requesting', requestData.url);
   });
+  const status = await page.open(`${url}${path}/${param}`, options);
+  // Status handler
+  let content: string;
+  if (status === 'success') content = await page.property('content');
+  else content = 'Requesting is fail.';
+  instance.exit();
+  // Return result
+  return JSON.stringify(content);
+}
 
-  // sset ttp operation & send data
+// [DELETE]
+async function sendDeleteRequest(path: string, param: string = ''): Promise<string> {
+  // Set operation
   const options: phantom.IOpenWebPageSettings = {
     operation: 'DELETE'
   }
-
-  const status = await page.open(`${baseUrl}/message/${id}`, options);
-  console.log(status);
-
-  const content = await page.property('content');
-  console.log(content);
-
-  await instance.exit();
+  // Set up request
+  const instance = await phantom.create();
+  const page = await instance.createPage();
+  await page.on('onResourceRequested', function (requestData) {
+    console.info('Requesting', requestData.url);
+  });
+  const status = await page.open(`${url}${path}/${param}`, options);
+  // Status handler
+  let content: string;
+  if (status === 'success') content = await page.property('content');
+  else content = 'Requesting is fail.';
+  instance.exit();
+  // Return result
+  return JSON.stringify(content);
 }
